5d30b885cb64dad15f62b13fe804ad4add69ae10
thenordplace <sadapan@gmail.com>
2023-01-18 18:32:18 -0700

    starting point for getting and saving notes

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const PORT = 3000;
 const app = express();
 
 
-const writeToFile = () => {
-  let dataPath = (path.join(__dirname,"./db/db.json"))
-  fs.writeFile(dataPath, JSON.stringify(notes, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${notes}`)
-  )};
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -28,9 +23,11 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => res.json(notes));
+//maybe this?
 app.post('/api/notes', (req,res) => {
   const { title, notes } = req.body;
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
